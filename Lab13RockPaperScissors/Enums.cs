7c52c78962cd6b37f0e7b054557ea1b5462cd7a1
Steve Schaner <schanerst@gmail.com>
2021-12-13 13:57:58 -0500

    Lab 13's welcome, name method, and enum complete.

---
+++ Lab13RockPaperScissors/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab13RockPaperScissors
+{
+    enum RPS
+    {
+        rock = 1,
+        paper,
+        scissors
+    }
+}
---
+++ Lab13RockPaperScissors/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab13RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title();
+            Welcome();
+            string name = GetusersName();
+
+            Exit();
+        }
+
+        public static void Title()
+        {
+            Console.Title = "Lab 13 - Rock, Paper, Scissors";
+        }
+
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome to Rock Paper Scissors!");
+        }
+
+        public static string GetusersName()
+        {
+            string name = "";
+            Console.Write("Enter your name: ");
+            return name = Console.ReadLine().Trim();
+        }
+
+        public static void Exit()
+        {
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+        
+    }
+}
